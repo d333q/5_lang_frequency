ca83814c59e72c0e1cdc6da2841a7205e292466c
d333q <d33q@yandex.ru>
2016-10-03 21:24:54 +0000

    finished

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,22 @@
+import os
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as text:
+        return text.read()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall('\w+', text)
+    return Counter(words).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    text = load_data(input('Введите названия файла '))
+    print ('''10 самых встречаемых слов в данном тексте
+{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}\n{9}'''.format(
+        *get_most_frequent_words(text)))
