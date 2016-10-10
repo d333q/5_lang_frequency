970cb67179bfee6322e2bd6808acd69cda295765
d333q <d33q@yandex.ru>
2016-10-10 21:11:24 +0000

    corrected

--- lang_frequency.py
+++ lang_frequency.py
@@ -14,18 +14,11 @@ def load_data(filepath):
 def get_most_frequent_words(text):
     lowercase = text.lower()
     words = re.findall('\w+', lowercase)
-    amountwordsshow = 10
-    frequencywords = Counter(words).most_common(amountwordsshow)
-    return frequencywords
-
-
-def print_words_count(frequencywords):
-    print('10 часто используемых слов в данном тексте')
-    for number in frequencywords:
-        print (number)
+    amount_words = 10
+    frequency_words = Counter(words).most_common(amount_words)
+    return frequency_words
 
 
 if __name__ == '__main__':
     text = load_data(input('Введите названия файла '))
-    frequencywords = get_most_frequent_words(text)
-    print_words_count(frequencywords)
+    print(get_most_frequent_words(text))
